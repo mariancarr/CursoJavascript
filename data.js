e555439d810c5366f164621a758f7c2a492c9301
MarianoCarrillo <carr.mariano@gmail.com>
2022-03-10 13:05:34 -0300

    pruebas

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const productos = []
-
+let carrito = []
 class inventario{
     constructor(id, producto, ingredientes, tamano, precio, stock, vegetariana){
         this.id = id.toFixed();
@@ -14,11 +14,11 @@ class inventario{
 productos.push(new inventario(1, "pizza", "muzzarella", "grande", 800, 15, "vegetariana"))
 productos.push(new inventario(2, "pizza", "jamon y muzzarella", "grande", 970, 15, "no"))
 productos.push(new inventario(3, "pizza", "fugaza", "grande", 900, 15, "vegetariana"))
-productos.push(new inventario(4, "pizza", "jamon y morron", "grande", 1020, 15, "no"))
+productos.push(new inventario(4, "pizza", "jamon y morron", "grande", 1020, 0, "no"))
 productos.push(new inventario(5, "pizza", "margarita", "grande", 900, 15, "vegetariana"))
 productos.push(new inventario(6, "pizza", "napolitana", "grande", 950, 15, "vegetariana"))
 productos.push(new inventario(7, "empanada", "jamon y queso", "unidad", 100, 15, "no"))
-productos.push(new inventario(8, "empanada", "cebolla y queso", "unidad", 105, 15, "vegetariana"))
+productos.push(new inventario(8, "empanada", "cebolla y queso", "unidad", 105, 0, "vegetariana"))
 productos.push(new inventario(9, "empanada", "carne suave", "unidad", 105, 15, "no"))
 productos.push(new inventario(10, "empanada", "pollo", "unidad", 110, 15, "no"))
 productos.push(new inventario(11, "empanada", "humita", "unidad", 105, 15, "vegetariana"))
@@ -31,6 +31,6 @@ productos.push(new inventario(17, "bebida", "coca-Cola", "500ml", 170, 15, "vege
 productos.push(new inventario(18, "bebida", "sprite", "500ml", 170, 15, "vegetariana"))
 productos.push(new inventario(19, "bebida", "fanta", "500ml", 170, 15, "vegetariana"))
 productos.push(new inventario(21, "bebida", "pomelo", "500ml", 170, 15, "vegetariana"))
-productos.push(new inventario(22, "postre", "volcan chocolate", " ", 375, 15, "vegetariana"))
-productos.push(new inventario(23, "postre", "cheesecake", " ", 350, 15, "vegetariana"))
+productos.push(new inventario(22, "postre", "volcan chocolate", " ", 375, 0, "vegetariana"))
+productos.push(new inventario(23, "postre", "cheesecake", " ", 350, 0, "vegetariana"))
 productos.push(new inventario(24, "postre", "flan con crema", " ", 300, 15, "vegetariana"))
--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+let rango =[]
+let maximo = 0
+let minimo = 0
+
 function init(){
     cargarProductos()
-   
+    btnAccionRango()
 }
 
 function cargarProductos(){
     const nodoProductos = document.getElementById("listaProductos")
     const table = document.createElement("table")
+    nodoProductos.innerHTML = ""
     table.setAttribute("id","listaInventario")
     table.setAttribute("class","table align-middle table-sm table-hover table-dark table-bordered")
 
@@ -22,41 +27,67 @@ function cargarProductos(){
                     </tr>
                 </thead>`
 
-    const tbody = document.createElement("tbody")
-    productos.forEach((producto)=>{
-        const tr = document.createElement("tr")
-        tr.innerHTML = `<td>${producto.id}</td>
-                        <td class="table-active">${producto.producto}</td>
-                        <td class="table-active">${producto.ingredientes}</td>
-                        <td>${producto.tamano}</td>
-                        <td class="table-active">${"$ "+ producto.precio}</td>
-                        <td>${producto.vegetariana}</td>
-                        <td>
-                            <input type="number" class="form-control d-flex justify-content-center align-items-center" id=canitdad style= width:50px></td>
-                        <td>
-                            <button class="btn btn-success" id="agregar">
-                                Agregar producto
-                            </button>
-                            <button class="btn btn-danger" id="eliminar">
-                                Eliminar
-                            </button>
-                        </td>
-                        `
-        tbody.append(tr)
-    })
-    table.append(tbody)
-    nodoProductos.append(table) 
+    const tbody = document.createElement("tbody")  
+        productos.forEach((producto)=>{
+            const tr = document.createElement("tr")
+            tr.innerHTML = `<td>${producto.id}</td>
+                            <td class="table-active">${producto.producto}</td>
+                            <td class="table-active">${producto.ingredientes}</td>
+                            <td>${producto.tamano}</td>
+                            <td class="table-active">${"$ "+ producto.precio}</td>
+                            <td>${producto.vegetariana}</td>
+                            <td><input type="number" class="form-control d-flex justify-content-center align-items-center" id=canitdad style= width:50px></td>
+                            <td>
+                                ${disponible(producto.id)}
+                            </td>   
+                            <td>
+                                <button class="btn btn-danger" id="eliminar">
+                                    Eliminar
+                                </button>
+                            </td>`
+            tbody.append(tr)
+            
+        })
+        table.append(tbody)
+        nodoProductos.append(table) 
+    }
+      
 
-    let minimo = document.getElementById("precioMinimo")
-    minimo.onchange = () =>{console.log(minimo.value)}
 
-    let maximo = document.getElementById("precioMaximo")
+
+function disponible(id){
+    const stockDisponible = productos.find((producto) =>  producto.id === id)
+    if (stockDisponible.stock === 0){
+        return "No disponible"
+    }
+
+    else{
+        return  `<button class="btn btn-success" id="agregar">
+                        Agregar
+                </button>`
+    }
+}
+
+function btnAccionRango(){
+    const btnRangos = document.getElementById("buscarPrecios")
+    btnRangos.addEventListener("click", () => {tomarValores()})
+}
+
+function tomarValores(){
+    minimo = document.getElementById("precioMinimo").value
+    maximo = document.getElementById("precioMaximo").value
+        if (minimo < 0 || maximo < 0){
+            alert("ERROR: Valores ingresados invalidos")
+        }
+        
+        else {
+            rangoPrecios()
+        }
+}
+
+function rangoPrecios(){
+    rango= productos.filter((p) => p.precio <= maximo && p.precio >= minimo)
     
-    maximo.onchange = () =>{let multi = (maximo.value)*3
-                            console.log(multi)
-                            }
-    
-    
-    
+    cargarProductos()
 }
 
