698e78f1252b558cf2c9ff055113d8e4e7f5e741
MarianoCarrillo <carr.mariano@gmail.com>
2022-03-12 15:03:32 -0300

    errores al ordenar o filtrar array

--- scripts/data.js
+++ scripts/data.js
@@ -1,35 +1,270 @@
-let productos = []
+
 let carrito = []
-class inventario{
-    constructor(id, producto, especificaciones, color, precio, stock, img){
-        this.id = id.toFixed();
-        this.producto = producto.toUpperCase();
-        this.especificaciones = especificaciones;
-        this.color = color.toLowerCase();
-        this.precio = parseFloat(precio);
-        this.stock = parseInt(stock);
-        this.img = img;
+
+class Inventario{
+    constructor(inventario, cantidad){
+        this.id = inventario.id;
+        this.producto = inventario.producto;
+        this.especificaciones = inventario.especificaciones;
+        this.color = inventario.color;
+        this.precio = inventario.precio;
+        this.stock = inventario.stock-1;
+        this.img = inventario.img;
+        this.cantidad = cantidad;
+        this.precioTotal = inventario.precio
+    }
+
+    agregarUnidad(){
+        this.cantidad++;
+        this.stock--;
+    }
+    
+    eliminarUnidad(){
+        this.cantidad--;
+        this.stock++;
+    }
+    
+    actualizarPrecioTotal(){
+        this.precioTotal = this.precio * this.cantidad;
     }
 }
 
-productos.push(new inventario(1, "AURICULARES", "HyperX Cloud Alpha Gaming Red PC", "red", 11500, 15, "images/auriculares_HyperX_Cloud_Alpha_Gaming_Red_PC.jpg"))
-productos.push(new inventario(2, "AURICULARES", "HyperX Cloud Stinger Gaming Negro PC", "black", 8700, 15, "images/auriculares_HyperX_Cloud_Stinger_Gaming_Negro__PC.jpg"))
-productos.push(new inventario(3, "AURICULARES", "Inalambricos Logitech G733 Lightspeed RGB White", "white", 9500, 15, "images/auriculares_Inalambricos_Logitech_G733_Lightspeed_RGB_White.jpg"))
-productos.push(new inventario(4, "AURICULARES", "Logitech PRO X Gaming", "black", 1020, 0, "images/auriculares_Logitech_PRO_X_Gaming.jpg"))
-productos.push(new inventario(5, "GABINETE", "ASUS ROG STRIX Helios Aluminum Black RGB", "black", 900, 15, "images/gabinete_ASUS_ROG_STRIX_Helios_Aluminum_Black_RGB.jpg"))
-productos.push(new inventario(6, "GABINETE", "Be Quiet Dark Base Pro 900 Black Rev 2", "black", 950, 15, "images/gabinete_Be_Quiet__DARK_BASE_PRO_900_Black_Rev_2.jpg"))
-productos.push(new inventario(7, "MONITOR", "Gamer ASUS ROG SWIFT 24 PG259QNR 360Hz 1Ms G-Sync", "unidad", 100, 15, "images/monitor_Gamer_ASUS_ROG_SWIFT_24__PG259QNR_360Hz_1Ms_G-Sync.jpg" ))
-productos.push(new inventario(8, "MONITOR", "Gamer ViewSonic 24 XG240R ELITE 144Hz 1ms", "unidad", 105, 0, "images/monitor_Gamer_ViewSonic_24__XG240R_ELITE_144Hz_1ms.jpg"))
-productos.push(new inventario(9, "MONITOR", "LG 32 UltraGear 32UN550 4K 60Hz", "unidad", 105, 15, "images/monitor_LG_32__UltraGear_32UN550_4K_60Hz.jpg"))
-productos.push(new inventario(10, "MOUSE", "Logitech G603 Lightspeed Wireless", "unidad", 110, 15, "images/mouse_Logitech_G603_Lightspeed_Wireless.jpg"))
-productos.push(new inventario(11, "MOUSE", "Logitech G703 Lightspeed Wireless", "unidad", 105, 15, "images/mouse_Logitech_G703_Lightspeed_Wireless.jpg"))
-productos.push(new inventario(12, "MOUSE", "Logitech G PRO X Superlight Black", "unidad", 105, 15, "images/mouse_Logitech_G_PRO_X_Superlight_Black.jpg"))
-productos.push(new inventario(13, "MOUSE", "Logitech G PRO X Superlight White", "unidad", 110, 15, "images/mouse_Logitech_G_PRO_X_Superlight_White.jpg"))
-productos.push(new inventario(14, "MOUSE PAD", "HyperX Fury S Pro Gaming Speed Edition L", "unidad", 105, 15, "images/mouse_Pad_HyperX_Fury_S_Pro_Gaming_Speed_Edition_L.jpg"))
-productos.push(new inventario(15, "MOUSE PAD", "Logitech G640 Gaming", "600ml", 150, 15,  "images/mouse_Pad_Logitech_G640_Gaming.jpg"))
-productos.push(new inventario(16, "MOUSE PAD", "Redragon Flick L", "600ml", 150, 15, "images/mouse_Pad_Redragon_Flick_L.jpg"))
-productos.push(new inventario(17, "TECLADO", "Logitech G213 Prodigy RGB", "500ml", 170, 15, "images/teclado_Logitech_G213_Prodigy_RGB.jpg"))
-productos.push(new inventario(18, "TECLADO MECANICO", "HyperX Alloy Origins RGB Switch Red", "500ml", 170, 15, "images/teclado_Mecanico_HyperX_Alloy_Origins_RGB_Switch_Red.jpg"))
-productos.push(new inventario(19, "TECLADO MECANICO", "Logitech PRO Series RGB GX Clicky Switches", "500ml", 170, 15, "images/teclado_Mecanico_Logitech_PRO_Series_RGB_GX_Clicky_Switches.jpg"))
-productos.push(new inventario(21, "WEBCAM", "Logitech C925e Full_HD con tapa", "500ml", 170, 15, "images/webCam_Logitech_C925e_Full_HD_con_tapa.jpg"))
-productos.push(new inventario(22, "WEBCAM", "Redragon GW800 Hitman 1080p", " ", 375, 0, "images/webcam_Redragon_GW800_Hitman_1080p.jpg"))
+let productos = [
+    {
+        id: 0, 
+        producto: "AURICULARES", 
+        marca: "Hyperx".toUpperCase(),
+        especificaciones: "HyperX Cloud Alpha Gaming Red PC".toUpperCase(), 
+        color:"rojo",
+        precio: 11500,
+        stock: 5,
+        img: "images/auriculares_HyperX_Cloud_Alpha_Gaming_Red_PC.jpg",       
+    },
+
+    {
+        id: 1, 
+        producto: "AURICULARES", 
+        marca: "Logitech".toUpperCase(),
+        especificaciones: "Inalambricos Logitech G733 Lightspeed RGB White".toUpperCase(),
+        color:"blanco",
+        precio: 12341,
+        stock: 3,
+        img: "images/auriculares_Inalambricos_Logitech_G733_Lightspeed_RGB_White.jpg",       
+    },
+
+    {
+        id: 2, 
+        producto: "AURICULARES", 
+        marca: "Hyperx".toUpperCase(),
+        especificaciones: "HyperX Cloud Stinger Gaming Negro PC".toUpperCase(),
+         color:"negro",
+        precio: 9000,
+        stock: 1,
+        img: "images/auriculares_HyperX_Cloud_Stinger_Gaming_Negro__PC.jpg",        
+    },
+
+    {
+        id: 3, 
+        producto: "AURICULARES", 
+        marca: "logitech".toUpperCase(),
+        especificaciones: "Logitech PRO X Gaming".toUpperCase(),
+         color:"negro",
+        precio: 5456,
+        stock: 4,
+        img: "images/auriculares_Logitech_PRO_X_Gaming.jpg",        
+    },
+
+    {
+        id: 4, 
+        producto: "GABINETE", 
+        marca: "ASUS".toUpperCase(),
+        especificaciones: "ASUS ROG STRIX Helios Aluminum Black RGB".toUpperCase(), 
+        color:"negro",
+        precio: 8766,
+        stock: 7,
+        img: "images/gabinete_ASUS_ROG_STRIX_Helios_Aluminum_Black_RGB.jpg",        
+    },
+
+    {
+        id: 5, 
+        producto: "GABINETE", 
+        marca: "redragon".toUpperCase(),
+        especificaciones: "Be Quiet Dark Base Pro 900 Black Rev 2".toUpperCase(), 
+        color:"red",
+        precio: 4345,
+        stock: 3,
+        img: "images/gabinete_Be_Quiet__DARK_BASE_PRO_900_Black_Rev_2.jpg",
+    },
+
+    {
+        id: 6, 
+        producto: "MONITOR", 
+        marca: "ASUS".toUpperCase(),
+        especificaciones: "Gamer ASUS ROG SWIFT 24 PG259QNR 360Hz 1Ms G-Sync".toUpperCase(),
+        color:"red",
+        precio: 6565,
+        stock: 8,
+        img: "images/monitor_Gamer_ASUS_ROG_SWIFT_24__PG259QNR_360Hz_1Ms_G-Sync.jpg",    
+    },
+
+    {
+        id: 7, 
+        producto: "MONITOR", 
+        marca: "ViewSonic".toUpperCase(),
+        especificaciones: "Gamer ViewSonic 24 XG240R ELITE 144Hz 1ms".toUpperCase(),
+        color:"red",
+        precio: 7676,
+        stock: 7,
+        img: "images/monitor_Gamer_ViewSonic_24__XG240R_ELITE_144Hz_1ms.jpg",     
+    },
+
+    {
+        id: 8, 
+        producto: "MONITOR", 
+        marca: "LG".toUpperCase(),
+        especificaciones: "LG 32 UltraGear 32UN550 4K 60Hz".toUpperCase(),
+        color:"red",
+        precio: 5347,
+        stock: 6,
+        img: "images/monitor_LG_32__UltraGear_32UN550_4K_60Hz.jpg",       
+    },
+
+    {
+        id: 9, 
+        producto: "MOUSE", 
+        marca: "Logitech".toUpperCase(),
+        especificaciones: "Logitech G603 Lightspeed Wireless".toUpperCase(),
+        color:"red",
+        precio: 8776,
+        stock: 11,
+        img: "images/mouse_Logitech_G603_Lightspeed_Wireless.jpg",     
+    },
+
+    {
+        id: 10, 
+        producto: "MOUSE", 
+        marca: "Logitech".toUpperCase(),
+        especificaciones: "Logitech G703 Lightspeed Wireless".toUpperCase(),
+        color:"red",
+        precio: 9999,
+        stock: 10,
+        img: "images/mouse_Logitech_G703_Lightspeed_Wireless.jpg",    
+    },
+
+    {
+        id: 11, 
+        producto: "MOUSE", 
+        marca: "Logitech".toUpperCase(),
+        especificaciones: "Logitech G PRO X Superlight Black".toUpperCase(),
+        color:"red",
+        precio: 545,
+        stock: 12,
+        img: "images/mouse_Logitech_G_PRO_X_Superlight_Black.jpg",    
+    },
+
+    {
+        id: 12, 
+        producto: "MOUSE", 
+        marca: "Logitec".toUpperCase(),
+        especificaciones: "Logitech G PRO X Superlight White".toUpperCase(),
+        color:"red",
+        precio: 1234,
+        stock: 14,
+        img: "images/mouse_Logitech_G_PRO_X_Superlight_White.jpg",    
+    },
+
+    {
+        id: 13, 
+        producto: "MOUSE PAD", 
+        marca: "Hyperx".toUpperCase(),
+        especificaciones: "HyperX Fury S Pro Gaming Speed Edition L".toUpperCase(),
+        color:"red",
+        precio: 2444,
+        stock: 15,
+        img: "images/mouse_Pad_HyperX_Fury_S_Pro_Gaming_Speed_Edition_L.jpg",  
+    },
+
+    {
+        id: 14, 
+        producto: "MOUSE PAD", 
+        marca: "Logitech".toUpperCase(),
+        especificaciones: "Logitech G640 Gaming".toUpperCase(),
+        color:"red",
+        precio: 1234,
+        stock: 13,
+        img: "images/mouse_Pad_Logitech_G640_Gaming.jpg",  
+    },
+
+    {
+        id: 15, 
+        producto: "MOUSE PAD", 
+        marca: "Redragon".toUpperCase(),
+        especificaciones: "Redragon Flick L".toUpperCase(),
+        color:"red",
+        precio: 1234,
+        stock: 12,
+        img: "images/mouse_Pad_Redragon_Flick_L.jpg",   
+    },
+
+    {
+        id: 16, 
+        producto: "TECLADO", 
+        marca: "Logitech".toUpperCase(),
+        especificaciones: "Logitech G213 Prodigy RGB".toUpperCase(),
+        color:"red",
+        precio: 1234,
+        stock: 7,
+        img: "images/teclado_Logitech_G213_Prodigy_RGB.jpg",  
+    },
+
+    {
+        id: 17, 
+        producto: "TECLADO", 
+        marca: "Hyperx".toUpperCase(),
+        especificaciones: "HyperX Alloy Origins RGB Switch Red".toUpperCase(),
+        color:"red",
+        precio: 1234,
+        stock: 8,
+        img: "images/teclado_Mecanico_HyperX_Alloy_Origins_RGB_Switch_Red.jpg",  
+    },
+
+    {
+        id: 18, 
+        producto: "TECLADO", 
+        marca: "Logitech".toUpperCase(),
+        especificaciones: "Logitech PRO Series RGB GX Clicky Switches".toUpperCase(),
+        color:"red",
+        precio: 1234,
+        stock: 9,
+        img: "images/teclado_Mecanico_Logitech_PRO_Series_RGB_GX_Clicky_Switches.jpg",   
+    },
+
+    {
+        id: 19, 
+        producto: "WEBCAM", 
+        marca: "Logitech".toUpperCase(),
+        especificaciones: "Logitech C925e Full_HD con tapa".toUpperCase(),
+        color:"red",
+        precio: 1234,
+        stock: 5,
+        img: "images/webCam_Logitech_C925e_Full_HD_con_tapa.jpg",   
+    },
+
+    {
+        id: 20, 
+        producto: "WEBCAM", 
+        marca: "Redragon".toUpperCase(),
+        especificaciones: "Redragon GW800 Hitman 1080p".toUpperCase(),
+        color:"red",
+        precio: 1234,
+        stock: 6,
+        img: "images/webcam_Redragon_GW800_Hitman_1080p.jpg",   
+    }
+]
+
+
+
+
+
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,11 @@
-let rango =[]
-let maximo = 0
-let minimo = 0
 
 function init(){
     cargarProductos()
-    btnAccionRango()
-    btnAccionFiltro()
+    btnAccion()
+    
 }
+
+
 
 function cargarProductos(){
     const nodoProductos = document.getElementById("gridProductos")
@@ -23,34 +22,30 @@ function cargarProductos(){
                                 <p class = "card-text"> ${producto.especificaciones}</p>
                                 <span> $ ${producto.precio}</span>
                                 <div class="buttonsProducto">
-                                    ${disponible(producto.id)}
-                                    <button class="btn btn-danger" id="eliminar">
+                                    <button class="btn btn-success" id="agregar${producto.id}">
+                                        Agregar
+                                    </button>
+                                    <button class="btn btn-danger" id="eliminar${producto.id}">
                                         Eliminar
                                     </button>
                                 </div>
                             </div>
                         </div>`
                                      
-        nodoProductos.appendChild(col)                           
+        nodoProductos.appendChild(col) 
+
+        const btnAgregar = document.getElementById(`agregar${producto.id}`)
+        btnAgregar.addEventListener("click", () => {agregarCarrito(producto.id)})           
+        
+        const btnEliminar = document.getElementById(`eliminar${producto.id}`)
+        btnEliminar.addEventListener("click", () => {eliminarCarrito(producto.id)})                     
     })
+}
       
-}
 
 
-function disponible(id){
-    const stockDisponible = productos.find((producto) =>  producto.id === id)
-    if (stockDisponible.stock === 0){
-        return "Sin stock"
-    }
 
-    else{
-        return  `<button class="btn btn-success" id="agregar">
-                        Agregar
-                </button>`
-    }
-}
-
-function btnAccionRango(){
+function btnAccion(){
     const btnRangos = document.getElementById("buscarPrecios")
     btnRangos.addEventListener("click", () => {tomarValores()})
 
@@ -59,6 +54,163 @@ function btnAccionRango(){
 
     const btnAscendente = document.getElementById("ordenarAscendente")
     btnAscendente.addEventListener("click", () => {ordenarAscendente()})
+
+    
+    const btnFiltro = document.getElementById("btnBuscarCoincidencias")
+    btnFiltro.addEventListener("click", () => {buscarCoincidencias()})
+  
+}
+
+
+
+
+function cargarCarrito(array){
+    let divCarrito = document.getElementById("carrito")
+    divCarrito.innerHTML =""
+    let precioTotal = obtenerPrecioTotal(array)
+
+    let cardTotal = document.createElement("div")
+    cardTotal.setAttribute("class", "row")
+    cardTotal.setAttribute("id", "rowTotal")
+    cardTotal.innerHTML = ` <h3 class="tituloCarrito">CARRITO DE COMPRAS</h3>
+                            <div class="row">
+                                <div class="col" id="btnVaciarCarrito">
+                                    <button type="button" class="btn btn-danger" id="vaciarCarrito">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+                                        </svg>
+                                        Vaciar Carrito
+                                    </button>
+                                </div>
+                                <div class="col" id="precioTotalCarrito">
+                                    <span id="totalTxt">TOTAL: &nbsp &nbsp</span> <span id="totalTxtPrecio">$${precioTotal.toLocaleString('es-AR', { minimumFractionDigits: 2})}</span>
+                                </div>
+                            </div>`
+
+    divCarrito.appendChild(cardTotal)
+
+    array.forEach((c)=>{
+        let card = document.createElement("div")
+        card.setAttribute("class", "card mt-4")
+        card.setAttribute("id", "carritoCard")
+        card.innerHTML = 
+                        `
+                            <div class= "row" id="rowCarrito">
+                                <div class= "col-3" id="imgCarrito">
+                                    <img src = "${c.img}" class="img-fluid">
+                                </div>
+                                <div class= "col-6" >
+                                    <div class = "card-body" id="productoCarrito">
+                                        <div class="textoCarrito">
+                                            <h5 class="card-title">${c.producto}</h5>
+                                            <p class = "card-text"> ${c.especificaciones}</p>
+                                        </div>    
+                                        
+                                        <div class="buttonsCarrito">
+                                            <div class="btnsC">
+                                                <button type="button" class="btn btn-light" id="eliminarCarrito${c.id}">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-lg" viewBox="0 0 16 16">
+                                                        <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z"/>
+                                                    </svg>
+                                                </button>
+
+                                                <span>${c.cantidad}</span>
+
+                                                <button type="button" class="btn btn-light" id="agregarCarrito${c.id}">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                                        <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                            <div id="precioCarritoUnidad">
+                                                <span> $ ${c.precioTotal}</span>
+                                            </div>    
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>    
+                        `
+                                     
+        divCarrito.appendChild(card)   
+
+        let btnEliminarCarrito = document.getElementById(`eliminarCarrito${c.id}`)
+        btnEliminarCarrito.addEventListener("click",() =>{ eliminarCarrito(c.id)})
+
+        let btnAgregarrCarrito = document.getElementById(`agregarCarrito${c.id}`)
+        btnAgregarrCarrito.addEventListener("click",() =>{ agregarCarrito(c.id)})
+
+        let btnVaciarCarrito = document.getElementById("vaciarCarrito")
+        btnVaciarCarrito.addEventListener("click",() => vaciarCarrito())
+        
+    })
+    
+}
+
+function vaciarCarrito(){
+    carrito = []
+    cargarCarrito(carrito)
+}
+
+function agregarCarrito(idProducto){
+    let productoEnCarrito = carrito.find((elemento) => elemento.id === idProducto)
+
+    if(productoEnCarrito){
+       let idx = carrito.indexOf(productoEnCarrito)
+       if(productoEnCarrito.stock === 0){
+            alert("No hay Stock")
+            cargarCarrito(carrito)
+       } 
+
+       else{
+           carrito[idx].agregarUnidad()
+           carrito[idx].actualizarPrecioTotal()
+       } 
+       
+    }
+
+    else{
+        if(productos.stock === 0){
+            alert("No hay Stock") 
+            cargarCarrito(carrito)
+           } 
+
+        else{
+            carrito.push(new Inventario(productos[idProducto],1))
+        }
+    }
+
+    localStorage.setItem("carritoStorage", JSON.stringify(carrito))
+    cargarCarrito(carrito)
+    console.log(carrito)
+}
+
+function eliminarCarrito(idProducto){
+    let productoEnCarrito = carrito.find((elemento) => elemento.id === idProducto)
+    let idx = carrito.findIndex((elemento) =>elemento.id === productoEnCarrito.id)
+    
+    if(productoEnCarrito.cantidad > 1){
+        carrito[idx].eliminarUnidad()
+        carrito[idx].actualizarPrecioTotal()
+    }
+
+    else{
+        carrito.splice(idx,1)
+    }
+
+    localStorage.setItem("carritoStorage", JSON.stringify(carrito))
+    cargarCarrito(carrito)
+}
+
+function obtenerPrecioTotal(array){
+    precioTotalCarrito = 0
+
+    for (const producto of array){
+        precioTotalCarrito += producto.precio * producto.cantidad
+    }
+    
+    return precioTotalCarrito
+    
 }
 
 function tomarValores(){
@@ -75,28 +227,30 @@ function tomarValores(){
 
 function rangoPrecios(){
     rango = productos.filter((p) => p.precio <= maximo && p.precio >= minimo)
-    rango.forEach((r) =>
-    console.table(r.producto, r.especificaciones, r.color,":.............$",  r.precio))
+    productos = rango
+    cargarProductos()
 }
 
-function btnAccionFiltro(){
-    const btnFiltro = document.getElementById("btnBuscarCoincidencias")
-    btnFiltro.addEventListener("click", () => buscarCoincidencias())
-}
 
 function buscarCoincidencias(){
     let filtro = document.getElementById("buscarCoincidencias").value
     productos = productos.filter((p) =>{
-        return p.producto.includes(filtro) || p.especificaciones.includes(filtro) || p.color.includes(filtro)})
+        return p.producto.includes(filtro.toUpperCase()) || p.especificaciones.includes(filtro.toUpperCase()) || p.color.includes(filtro.toUpperCase())})
+    if (productos.length === 0){
+        console.log(productos)
+        cargarProductos()
+    }
+    else{
+        cargarProductos()
+    }
     
-    cargarProductos()
                         
 }
 
 
-
 function ordenarDescendente(){
-    productos =  productos.sort((a, b) => b.precio - a.precio)
+    let rango =  productos.sort((a, b) => b.precio - a.precio)
+    productos = rango
     cargarProductos()
 }
 
